644406d19927eb5aded3009ded425c731735a950
Vishalsahu0402 <124240284+Vishalsahu0402@users.noreply.github.com>
2024-02-29 13:08:17 +0530

    Update Skills.jsx

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -17,47 +17,36 @@ const Skills = () => {
                                 <div class="col-3 text-danger text-center ">
                                     <div class="bg-light images" >
                                         <img src="/images/html.png" alt="" />
-                                        <p>90%</p>
-
                                     </div>
                                         <p className='skillsname'>HTML</p>
                                 </div>
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/css.png" alt="" />
-                                        <p>90%</p>
-
-                                    </div>
+                                         </div>
                                     <p className='skillsname'>CSS</p>
                                 </div>
 
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/JS.png" alt="" />
-                                        <p>80%</p>
+                                
                                     </div>
                                     <p className='skillsname'>JavaScript</p>
                                 </div>
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/React.png" alt="" />
-                                        <p>80%</p>
-
-
-                                    </div>
+                                     </div>
                                     <p className='skillsname'>React.js</p>
 
                                 </div>
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/next.png" alt="" />
-                                        <p>70%</p>
-
-                                    </div>
+                                     </div>
                                     <p className='skillsname'>Next.js</p>
-
                                 </div>
-
                             </div>
                         </div>
                         <div className="col-md-6">
@@ -66,25 +55,18 @@ const Skills = () => {
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/nodejs.png" alt="" />
-                                        <p>70%</p>
-
                                     </div>
                                     <p className='skillsname'>Node.Js</p>
-
                                 </div>
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/express.png" alt="" />
-                                        <p>70%</p>
-
                                     </div>
                                     <p className='skillsname'>Express.js</p>
-
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     <div className='row my-md-5'>
                         <div className="col-md-6">
                             <h5 className='skilltypes pt-5 pt-md-0 mt-md-0'>Database</h5>
@@ -92,35 +74,25 @@ const Skills = () => {
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/mongoodb.png" alt="" />
-                                        <p>70%</p>
-
                                     </div>
                                     <p className='skillsname'>MongoDB</p>
-
                                 </div>
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/sql.png" alt="" />
-                                        <p>80%</p>
-
                                     </div>
                                     <p className='skillsname'>MySQL</p>
-
                                 </div>
                             </div>
                         </div>
-
                         <div className="col-md-6">
-
                             <h5 className='skilltypes pt-5 pt-md-0'>Others</h5>
                             <div class="row gy-3 pe-md-5">
                                 <div class="col-3 text-danger text-center">
                                     <div class="bg-light images" >
                                         <img src="/images/restapi.png" alt="" />
-                                        <p>80%</p>
                                     </div>
                                     <p className='skillsname'>REST API</p>
-
                                 </div>
                             </div>
                         </div>
