a5bbd08ed21d135c37c3dbc9a55e34cc6f9f102a
Vishal sahu <vishalsahu1230@gmail.com>
2025-06-02 07:04:13 +0530

    issue fixed

--- src/intro/Intro.jsx
+++ src/intro/Intro.jsx
@@ -3,22 +3,24 @@ import './intro.css'
 const Intro = () => {
     const openInNewTab = (url) => {
         window.open(url, '_blank', 'noreferrer');
-      };        
+    };
     return (
         <div className='container py-3 '>
             <div className='row mb-2'>
                 <div className='col-md-6 align-self-center' >
                     <h2 className='heading '>Hi, I am <br />
-                        <span>vishal Sahu</span> </h2> 
-                        <p className='introDesc'>
+                        <span>vishal Sahu</span> </h2>
+                    <p className='introDesc'>
                         A passionate Full Stack Web Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Expressjs / MongoDB and some other cool libraries and frameworks.
-                            </p>
-                            <button className='resumeBTN p-md-2 mt-3' role="link"
-        onClick={() =>openInNewTab('https://drive.google.com/file/d/1z51Lm70QIGE_TupaZNFjqcn6lFlJxQ_A/view?usp=sharing')}>Resume</button>
-                            </div>
+                    </p>
+                    <button className='resumeBTN p-md-2 mt-3' role="link"
+                        onClick={() =>  window.open(`/Vishal_Sahu__Resume.pdf`, '_blank')}>
+                        Resume
+                    </button>
+                </div>
                 <div className='col-md-6 ' >
                     <img className='ProfileImage' src="/images/profile.png" alt="" />
-                     </div>
+                </div>
             </div>
 
         </div>
--- src/workexperience/Workexp.jsx
+++ src/workexperience/Workexp.jsx
@@ -14,32 +14,34 @@ const Workexp = () => {
                         {/* Section: Timeline */}
                         <section className="pt-5">
                             <ul className="timeline">
+
+
                                 <li className="timeline-item mb-5">
-                                    <h5 className="fw-bold expheadings">Software Developer Intern - </h5>
-                                    <p className="expDates">June 2022 - December 2022</p>
+                                    {/* <h5 className="fw-bold expheadings">Software Developer trainee - Aurasoft digitech</h5> */}
+                                    <h5 className="fw-bold expheadings">Mern Stack Developer  -  Bit Techno Solution</h5>
+                                    <p className="expDates">may 2023 - Present</p>
                                     <ul>
-                                        <li className="expPoints">Contributed in development of an <span> banking ATM application </span>with a features to credit ,withdraw, and manage account balance  for a <span> seamless user experience.</span></li>
-                                        <li className="expPoints">Utilized  <span> Java, html,css,and sql</span>  to create a modern and user-friendly interface.</li>
+                                        <li className="expPoints">Developed backend services using <span> Node.js and Express.js, reducing API response times by 40%</span>.</li>
+                                        <li className="expPoints">Integrated <span>Razorpay SDK for secure transaction handling, supporting over 10K monthly transactions</span>.</li>
+                                        <li className="expPoints">Designed <span>optimized MySQL schema and queries </span> for efficient data access</li>
+                                        <li className="expPoints">Ensured <span>99.9% uptime for APIs </span> through rigorous testing and debugging </li>
                                     </ul>
                                 </li>
                                 <li className="timeline-item mb-5">
                                     <h5 className="fw-bold expheadings">Software Developer Intern - Aurasoft digitech</h5>
-                                    <p className="expDates">feb 2023 - apr 2023</p>
+                                    <p className="expDates">Jan 2023 - apr 2023</p>
                                     <ul>
                                         <li className="expPoints">Leveraged <span> HTML, CSS, JavaScript, and Bootstrap </span>to design and implement the frontend of the website.</li>
                                         <li className="expPoints">Collaborated with a team of <span> 5 developers </span>to create a  college councelling platform.</li>
                                         <li className="expPoints">Utilized <span>reactJs, html,css,Redux,and bootstrap</span>  to create a modern and user-friendly interface.</li>
                                     </ul>
                                 </li>
-                                <li className="timeline-item ">
-                                    {/* <h5 className="fw-bold expheadings">Software Developer trainee - Aurasoft digitech</h5> */}
-                                    <h5 className="fw-bold expheadings">Mern Stack Developer  - Aurasoft digitech</h5>
-                                    <p className="expDates">may 2023 - oct 2023</p>
+                                <li className="timeline-item mb-5">
+                                    <h5 className="fw-bold expheadings">Software Developer Intern - Ypsilon IT Solution </h5>
+                                    <p className="expDates">June 2022 - December 2022</p>
                                     <ul>
-                                        <li className="expPoints">developed a <span>E-commerce web application </span> from scrach.</li>
-                                        <li className="expPoints">Utilized <span> reactJs,NextJs,Redux, html,css,and bootstrap </span> to create a modern and user-friendly interface.</li>
-                                        <li className="expPoints">Utilized <span> NodeJs and Express </span>to build a flexible and easy to understand Backend</li>
-                                        <li className="expPoints">Utilized <span> mongoDB</span> to manage and store data</li>
+                                        <li className="expPoints">Contributed in development of an <span> banking ATM application </span>with a features to credit ,withdraw, and manage account balance  for a <span> seamless user experience.</span></li>
+                                        <li className="expPoints">Utilized  <span> Java, html,css,and sql</span>  to create a modern and user-friendly interface.</li>
                                     </ul>
                                 </li>
 
